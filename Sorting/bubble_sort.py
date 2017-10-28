cd90a99536339d288e574796ddf532802b9790bf
Cho-PC\Cho <weapon114@naver.com>
2017-10-28 23:12:55 +0900

    <정렬 알고리즘> Bubble Sorting을 활용한 정렬 알고리즘

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,38 @@
+# Bubble Sort
+
+def swap(arr, first, second):
+    arr[first], arr[second] = arr[second], arr[first]
+
+
+def bubble_sort(argv):
+    for index in reversed(range(len(argv))):
+        for i in range(index):
+            if argv[i] > argv[i + 1]:  # 두번째 인자가 첫번째 인자보다 클 경우 Swapping
+                swap(argv, i, i + 1)
+    return argv
+
+
+a = []
+size = int(input('Input Size of List: '))  # Input List's size
+
+index = 0
+while (index < size):
+    data = int(input('Input List''s data: '))  # Input List's value
+    a.append(data)
+    index += 1
+
+# Before
+index = 0
+while (index < size):
+    print(a[index], end=' ')
+    index += 1
+
+print()
+# Sorting List
+bubble_sort(a)
+
+# After
+index = 0
+while (index < size):
+    print(a[index], end=' ')
+    index += 1
