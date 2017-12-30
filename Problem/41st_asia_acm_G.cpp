74644d576ed69e39c72623a054c23e30b83fb228
skCho-PC\skCho <weapon114@naver.com>
2017-12-30 13:39:26 +0900

    <ACM ICPC> 41st ASIA Regional Problem G (Percolation)

--- Problem/41st_asia_acm_G.cpp
+++ Problem/41st_asia_acm_G.cpp
@@ -1,5 +1,35 @@
-#include "Maze.h"
+﻿/**
+	The 41st Annual ACM
+	Asia Regional-Daejeon
+	ASIA Regional
+	
+	Probelm G (Percolation)
+*/
 
+#include <iostream>
+
+using namespace std;
+
+class Point {
+public:
+	int row;
+	int col;
+};
+
+class Maze {
+public:
+	Maze();
+	void createMap(int mapRow, int mapCol);
+	bool pathExist(Point src, Point dst);
+	bool pathExistRecur(Point curSrc, Point dst);
+	bool getVisited(int row, int col);
+
+private:
+	int mapRow;
+	int mapCol;
+	int** map;
+	bool** visited;
+};
 
 Maze::Maze() {}
 
@@ -9,21 +39,29 @@ Maze::Maze() {}
 	param 1: Column length in map
 	param 2: Row length in map
 */
-void Maze::createMap(int mapCol, int mapRow) {
-	this->mapCol = mapCol;
+void Maze::createMap(int mapRow, int mapCol) {
 	this->mapRow = mapRow;
+	this->mapCol = mapCol;
 
 	map = new int*[mapRow];
 	visited = new bool*[mapRow];
 
-	for (int i = 0; i < mapCol; i++) {
+	char k;
+
+	for (int i = 0; i < mapRow; i++) {
 		map[i] = new int[mapCol];
 		visited[i] = new bool[mapCol];
 	}
 
 	for (int i = 0; i < mapRow; i++) {
+		for (int j = 0; j < mapCol; j++)
+			visited[i][j] = false;
+	}
+
+	for (int i = 0; i < mapRow; i++) {
 		for (int j = 0; j < mapCol; j++) {
-			cin >> map[i][j];
+			cin >> k;
+			map[i][j] = int(k - 48);
 		}
 	}
 }
@@ -35,13 +73,7 @@ void Maze::createMap(int mapCol, int mapRow) {
 	param 2: Destination point
 */
 bool Maze::pathExist(Point src, Point dst) {
-	for (int i = 0; i < mapRow; i++) {
-		for (int j = 0; j < mapCol; j++) {
-			visited[i][j] = false;
-		}
-	}
-
-	if (map[src.row][src.col] == 1 || map[dst.row][dst.col] == 1)   // When the start or destination is blocked 
+	if (map[src.row][src.col] == 1)
 		return false;
 	else
 		return pathExistRecur(src, dst);
@@ -49,18 +81,19 @@ bool Maze::pathExist(Point src, Point dst) {
 
 /**
 	Recursive path search
-
+	
 	param 1: Current location point
 	param 2: Destination point
 */
 bool Maze::pathExistRecur(Point curSrc, Point dst) {
-	visited[curSrc.row][curSrc.col] = true;
-	if (curSrc.row == dst.row && curSrc.col == dst.col)
+	if (curSrc.row == dst.row)
 		return true;
 
-	Point nextSpot;
 
-	if (curSrc.row + 1 < mapRow) {  // Top navigation
+	visited[curSrc.row][curSrc.col] = true;
+
+	Point nextSpot;
+	if (curSrc.row + 1 < mapRow) {  // Down navigation
 		if (map[curSrc.row + 1][curSrc.col] == 0 && visited[curSrc.row + 1][curSrc.col] == false) {
 			nextSpot.row = curSrc.row + 1;
 			nextSpot.col = curSrc.col;
@@ -69,7 +102,7 @@ bool Maze::pathExistRecur(Point curSrc, Point dst) {
 		}
 	}
 
-	if (curSrc.row - 1 >= 0) {   // Down navigation
+	if (curSrc.row - 1 >= 0) {   // Top navigation
 		if (map[curSrc.row - 1][curSrc.col] == 0 && visited[curSrc.row - 1][curSrc.col] == false) {
 			nextSpot.row = curSrc.row - 1;
 			nextSpot.col = curSrc.col;
@@ -96,4 +129,34 @@ bool Maze::pathExistRecur(Point curSrc, Point dst) {
 		}
 	}
 	return false;
+}
+
+bool Maze::getVisited(int row, int col) {
+	return visited[row][col];
+}
+
+int main()
+{
+	Maze maze;
+	Point src, dst;
+
+	int mapRow, mapCol;
+	cin >> mapRow >> mapCol;
+	maze.createMap(mapRow, mapCol);
+
+	src.row = 0;
+	dst.row = mapRow - 1;
+
+	for (int start = 0; start < mapCol; start++) {  // All the topmost rows of the map
+		src.col = start;
+
+		if (!maze.getVisited(src.row, src.col)) { // False if you have already visited
+			if (maze.pathExist(src, dst)) {
+				cout << "YES" << endl;
+				return 0;
+			}
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
 }
