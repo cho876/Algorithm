108c4da83451f9a21ac55068e778e2d9eb258e5e
skCho-PC\skCho <weapon114@naver.com>
2017-12-31 12:35:51 +0900

    <ACM ICPC> 41st Nationwide Internet Competition Problem L (Trucks)

--- Problem/41st_internet_acm_L.cpp
+++ Problem/41st_internet_acm_L.cpp
@@ -1,62 +1,55 @@
-/**
+﻿/**
 	The 41st Annual ACM
-		Asia Regional-Daejeon
-		Nationwide Internet Competition
-		Probelm L (Trucks)
+	Asia Regional-Daejeon
+	Nationwide Internet Competition
+
+	Probelm L (Trucks)
 */
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-struct Truck {
-	int weight;
-	int pos;
-};
-
 int main()
 {
-	int number_of_trucks; // Number of trucks
-	int bridge_length;  // Bridge length
-	int maximum_load;  // Maximum load
+	queue<int> queue;
+
+	int number_of_trucks;
+	int bridge_length;
+	int maximum_load;
 
 	cin >> number_of_trucks >> bridge_length >> maximum_load;
 
-	Truck* truck = new Truck[number_of_trucks];
+	int tmp_truck_weight;
+	int result = 0;
+	int pos = 0;
+	int cur_load = 0;
 
 	for (int i = 0; i < number_of_trucks; i++) {
-		cin >> truck[i].weight;
-		truck[i].pos = 0;
-	}
+		cin >> tmp_truck_weight;
 
-	int number_of_trucks_past = 0;  // Number of Trucks Past
-	int cur = 0;                    // Current bus index
-	int next = 0;                   // Next bus index
-	int time = 0;                   // Elapsed time
-	int current_load = 0;           //Current load
+		while (1) {
+			result++;
 
-	while (number_of_trucks_past != number_of_trucks) {  // Until all the trucks pass the bridge
-		while (truck[cur].pos < bridge_length) {  // Until the current truck passes the bridge
-			time++;
-			if (truck[cur].pos == 0) {  // When the current truck enters the bridge
-				truck[cur].pos++;
-				current_load += truck[cur].weight;
-				next = cur + 1;
+			if (pos >= bridge_length) {
+				cur_load -= queue.front();
+				queue.pop();
+				pos--;
 			}
-			else if (truck[cur].pos != 0 && truck[next].weight + current_load <= maximum_load) {  // If you have already entered the bridge and an additional truck is available
-				for (int j = cur; j <= next; j++)
-					truck[j].pos++;
-				current_load += truck[next].weight;
-				next++;
+			if (cur_load + tmp_truck_weight > maximum_load) {
+				queue.push(0);
+				pos++;
 			}
-			else if (truck[cur].pos != 0 && truck[next].weight + current_load > maximum_load) {  // If you have already entered the bridge and no additional trucks can enter
-				for (int j = 0; j < next; j++)
-					truck[cur].pos++;
+			else {
+				queue.push(tmp_truck_weight);
+				cur_load += tmp_truck_weight;
+				pos++;
+				break;
 			}
 		}
-		current_load -= truck[cur].weight;  // Current load reduction as much as the weight of the truck that passed
-		cur++;
-		number_of_trucks_past++;
 	}
-	cout << time + 1 << endl;
+
+	cout << result + bridge_length << endl;
+	return 0;
 }
