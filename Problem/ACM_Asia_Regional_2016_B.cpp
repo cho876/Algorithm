ce2a9224c239564e98163f8128feeee13e7fee15
skCho-PC\skCho <weapon114@naver.com>
2018-01-10 17:40:13 +0900

    <ACM ICPC> Asis Regional - Daejeon 2016 Problem B (Football Game)

---
+++ Problem/ACM_Asia_Regional_2016_B.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool isCorrect(vector<int> v_games, int index, int curTotal);
+
+int main()
+{
+	int number_of_team;
+	cin >> number_of_team;
+
+	vector<int> v_teams;
+	int tmp_score;
+	for (int i = 0; i < number_of_team; i++) {
+		cin >> tmp_score;
+		v_teams.push_back(tmp_score);
+	}
+
+	sort(v_teams.begin(), v_teams.end());
+
+	if (isCorrect(v_teams, 0, 0))
+		cout << "1" << endl;
+	else
+		cout << "-1" << endl;
+
+	return 0;
+
+}
+
+bool isCorrect(vector<int> v_games, int index, int curTotal) {
+	if (index >= v_games.size()) {
+		if (curTotal == index * (index - 1) / 2) 
+			return true;
+		else
+			return false;
+	}
+	else {
+		curTotal += v_games[index];
+		if (curTotal >= index * (index + 1) / 2) {
+			return isCorrect(v_games, index + 1, curTotal);
+		}
+		else
+			return false;
+	}
+}
