2394e638914d73f8479bac4d74d6085900aab959
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 17:08:58 +0900

    <백준 문제풀이> 기타줄

---
+++ Problem/greedy/backjoon_1049.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Brand {
+	int package;
+	int single;
+};
+
+bool cmp_sort(Brand a, Brand b) { return a.single < b.single; }
+
+int main() {
+	int lines;
+	int type;
+	cin >> lines >> type;
+	vector<Brand> v_brand;
+
+	Brand brand;
+	for (int i = 0; i < type; i++) {
+		cin >> brand.package >> brand.single;
+		v_brand.push_back(brand);
+	}
+
+	sort(v_brand.begin(), v_brand.end(), cmp_sort);
+
+	int package_choice = 9999999;
+	for (int i = 0; i < type; i++) 
+		package_choice = min(v_brand[i].package, package_choice);
+
+	package_choice = min(v_brand[0].single * 6, package_choice);
+
+	int result = 0;
+	result = package_choice * (lines / 6);
+	lines %= 6;
+
+	if (v_brand[0].single * lines > package_choice)  
+		result += package_choice;
+	else 
+		result += v_brand[0].single * lines;
+
+	cout << result << endl;
+}
