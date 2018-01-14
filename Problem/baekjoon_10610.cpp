4a1474a15821dcfda0ad29f5538d18fdb0406a97
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 14:52:41 +0900

    <백준 문제풀이> 30

---
+++ Problem/baekjoon_10610.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool cmp_sort(int a, int b) { return a > b; }
+
+int main()
+{
+	string str_num;
+	cin >> str_num;
+	vector<int> v_list;
+
+	int sum = 0;
+	for (int i = 0; i < str_num.length(); i++) {
+		v_list.push_back(str_num[i] - '0');
+		sum += v_list[i];
+	}
+
+	if (sum % 3 == 0) {
+		sort(v_list.begin(), v_list.end(), cmp_sort);
+		if (v_list[v_list.size() - 1] == 0) {
+			for (int i = 0; i < v_list.size(); i++)
+				cout << v_list[i];
+			return 0;
+		}
+	}
+
+	cout << "-1" << endl;
+	return 0;
+}
