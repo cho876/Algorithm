228f496a50391de46daf3b950684bccf88ad5d87
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 14:24:37 +0900

    <백준 문제풀이> 로프

---
+++ Problem/backjoon_2217.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int total_ropes;
+	cin >> total_ropes;
+	vector<int> v_ropes;
+
+	int tmp_weight;
+	for (int i = 0; i < total_ropes; i++) {
+		cin >> tmp_weight;
+		v_ropes.push_back(tmp_weight);
+	}
+
+	sort(v_ropes.begin(), v_ropes.end());
+
+	int result = 0;
+
+	for (int i = 0; i < v_ropes.size(); i++) {
+		result = max(result, v_ropes[i] * (total_ropes - i));
+	}
+	cout << result << endl;
+
+	return 0;
+}
