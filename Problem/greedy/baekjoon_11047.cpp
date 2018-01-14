061bb6346071cf8b86fadce3c351f5410d2bd8b9
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 16:06:06 +0900

    <백준 문제풀이> 동전0

---
+++ Problem/greedy/baekjoon_11047.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp_coins(int a, int b) { return a > b; }
+void DFS(vector<int> v_coins, int index, int total_money, int& result);
+
+int main()
+{
+	int number_of_coins;
+	int limit_money;
+
+	cin >> number_of_coins >> limit_money;
+	vector<int> v_coins;
+
+	int type_of_money;
+	for (int i = 0; i < number_of_coins; i++) {
+		cin >> type_of_money;
+		v_coins.push_back(type_of_money);
+	}
+
+	sort(v_coins.begin(), v_coins.end(), cmp_coins);
+
+
+	int result = 0;
+	int index = 0;
+	while (limit_money != 0) {
+		if (v_coins[index] <= limit_money) {
+			limit_money -= v_coins[index];
+			result++;
+		}
+		else
+			index++;
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
