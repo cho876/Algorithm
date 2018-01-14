71798083ce23151309b02e0d3ae6c92c51b14b00
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 16:07:13 +0900

    <백준 문제풀이> 대회 or 인턴

---
+++ Problem/greedy/baekjoon_2875.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int getWomanCount(int woman) {
+	return woman/2;
+}
+
+int main() {
+	int woman, man, intern;
+	cin >> woman >> man >> intern;
+
+	int womanCount;
+	while (intern > 0) {
+		womanCount = getWomanCount(woman);
+		if (womanCount >= man)
+			woman -= 1;
+		else
+			man -= 1;
+		intern--;
+	}
+
+	cout << min(getWomanCount(woman), man) << endl;
+}
