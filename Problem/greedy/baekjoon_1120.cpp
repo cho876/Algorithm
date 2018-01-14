44f1b2bdc7a195113db46dc1d8fdb8d5a8cc79ca
skCho-PC\skCho <weapon114@naver.com>
2018-01-14 17:40:28 +0900

    <백준 문제풀이> 문자열

---
+++ Problem/greedy/baekjoon_1120.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string cmp1;
+	string cmp2;
+
+	cin >> cmp1 >> cmp2;
+
+	int count = 0;
+	int result = 999999;
+	for (int i = 0; i <= cmp2.length() - cmp1.length(); i++) {
+		count = 0;
+		for (int j = 0; j < cmp1.length(); j++) {
+			if (cmp1[j] != cmp2[i + j])
+				count++;
+		}
+		result = min(count, result);
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
