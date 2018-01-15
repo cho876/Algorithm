c0ae3c7abd6f644b65c76580b557fcd0105cc468
skCho-PC\skCho <weapon114@naver.com>
2018-01-15 19:39:53 +0900

    <백준 문제풀이> 반도체 설계

---
+++ Problem/greedy/baekjoon_2352.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int ports;
+	cin >> ports;
+
+	vector<int> v_port;
+	vector<int> v_result;
+
+	int port;
+	for (int i = 0; i < ports; i++) {
+		cin >> port;
+		v_port.push_back(port);
+	}
+
+	v_result.push_back(v_port[0]);
+	int portNum = v_port[0];
+	for (int i = 0; i < v_port.size(); i++) {
+		if (portNum < v_port[i]) 
+			v_result.push_back(v_port[i]);
+		else {
+			int iterNum = lower_bound(v_result.begin(), v_result.end(), v_port[i]) - v_result.begin();
+			v_result[iterNum] = v_port[i];
+		}
+		portNum = v_result.back();
+	}
+
+	cout << v_result.size() << endl;
+}
