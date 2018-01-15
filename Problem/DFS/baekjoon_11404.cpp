a38efdb03eee79ce6f00aaab7d75bf49835c19d4
skCho-PC\skCho <weapon114@naver.com>
2018-01-16 00:16:53 +0900

    <백준 문제풀이> 플로이드

---
+++ Problem/DFS/baekjoon_11404.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define INF 99999999
+
+using namespace std;
+
+int main()
+{
+	int nodes, edges;
+	cin >> nodes >> edges;
+	vector<int>* v_nodes = new vector<int>[nodes];
+
+	for (int i = 0; i < nodes; i++) {
+		v_nodes[i].resize(nodes);
+		for (int j = 0; j < nodes; j++)
+			v_nodes[i][j] = (i == j) ? 0 : INF;
+	}
+
+
+	int from, to, weight;
+	for (int i = 0; i < edges; i++) {
+		cin >> from >> to >> weight;
+		v_nodes[from - 1][to - 1] = min(v_nodes[from - 1][to - 1], weight);
+	}
+
+	for (int k = 0; k < nodes; k++) {
+		for (int i = 0; i < nodes; i++) {
+			for (int j = 0; j < nodes; j++)
+				v_nodes[i][j] = min(v_nodes[i][j], v_nodes[i][k] + v_nodes[k][j]);
+		}
+	}
+
+	for (int i = 0; i < nodes; i++) {
+		for (int j = 0; j < nodes; j++)
+			cout << v_nodes[i][j] << " ";
+		cout << endl;
+	}
+
+	return 0;
+}
