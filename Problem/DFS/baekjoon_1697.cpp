7cfc540540c51ecb1b30b931aa11cf9b4644bb5a
skCho-PC\skCho <weapon114@naver.com>
2018-01-15 22:50:48 +0900

    <백준 문제풀이> 숨바꼭질

---
+++ Problem/DFS/baekjoon_1697.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <queue>
+#include <cstdio>
+#include <algorithm>
+
+#define MAX 100000
+
+using namespace std;
+
+int main()
+{
+	int subin, sister;
+	cin >> subin >> sister;
+
+	bool isVisited[MAX] = { false };
+
+	queue<pair<int, int>> q_src;
+	q_src.push({ subin, 0 });
+
+	while (!q_src.empty()) {
+		int curSrc = q_src.front().first;
+		int time = q_src.front().second;
+		q_src.pop();
+
+		if (curSrc < 0 || curSrc > 100000)
+			continue;
+
+		if (isVisited[curSrc])
+			continue;
+
+		if (curSrc == sister) {
+			cout << time << endl;
+			break;
+		}
+		isVisited[curSrc] = true;
+
+
+		q_src.push({ curSrc * 2, time + 1 });
+		q_src.push({ curSrc + 1, time + 1 });
+		q_src.push({ curSrc - 1, time + 1 });
+	}
+
+	return 0;
+}
