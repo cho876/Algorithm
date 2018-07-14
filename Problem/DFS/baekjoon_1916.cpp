4b661923b55c99c830732fccbc31ed9f060052ed
skCho-PC\skCho <weapon114@naver.com>
2018-07-15 00:26:15 +0900

    <백준 문제풀이> 최소비용 구하기

--- Problem/DFS/baekjoon_1916.cpp
+++ Problem/DFS/baekjoon_1916.cpp
@@ -2,61 +2,68 @@
 #include <cstdio>
 #include <vector>
 #include <queue>
+#include <functional>
 
-#define INF 9999999999
+#define INF 99999999
 
 using namespace std;
 
-void Dijkstra(vector<pair<int, int> >* v_pair, int src, int dst, int size);
 
-int main()
-{
+int Dijkstra(vector<pair<int, int> >* v_adj, bool*& isVisited, int nodes, int src, int dst) {
+	vector<int> dist;
+	dist.resize(nodes);
+
+	fill(dist.begin(), dist.end(), INF);
+	dist[src] = 0;
+
+	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > path;
+	path.push(make_pair(0, src));
+
+	int curNode, curWeight, nextNode, nextWeight;
+	while (!path.empty()) {
+		curWeight = path.top().first;
+		curNode = path.top().second;
+		path.pop();
+
+		if (curNode == dst)
+			return dist[dst];
+
+		isVisited[curNode] = true;
+		
+		for (int i = 0; i < v_adj[curNode].size(); i++) {
+			nextNode = v_adj[curNode][i].first;
+			nextWeight = curWeight + v_adj[curNode][i].second;
+
+			if (isVisited[nextNode])
+				continue;
+
+			if (dist[nextNode] > nextWeight) {
+				dist[nextNode] = nextWeight;
+				path.push(make_pair(nextWeight, nextNode));
+			}
+		}
+	}
+}
+
+int main() {
 	int nodes, edges;
 	scanf("%d%d", &nodes, &edges);
 
-	vector<pair<int, int> >* v_pair = new vector<pair<int, int> >[nodes];
+	vector<pair<int, int>>* v_adj = new vector<pair<int, int> >[nodes];
 
 	int from, to, weight;
 	for (int i = 0; i < edges; i++) {
 		scanf("%d%d%d", &from, &to, &weight);
-		v_pair[from - 1].push_back(make_pair(to - 1, weight));
+		v_adj[from - 1].push_back(make_pair(to - 1, weight));
 	}
+
+	bool* isVisited = new bool[nodes];
+	fill(isVisited, isVisited + nodes, false);
 
 	int src, dst;
 	scanf("%d%d", &src, &dst);
 
-	Dijkstra(v_pair, src - 1, dst - 1, nodes);
+	printf("%d\n", Dijkstra(v_adj, isVisited, nodes, src - 1, dst - 1));
 
 	return 0;
-}
-
-void Dijkstra(vector<pair<int, int> >* v_pair, int src, int dst, int size) {
-	vector<int> dist;
-	dist.resize(size);
-	for (int i = 0; i < size; i++)
-		dist[i] = INF;
-	dist[src] = 0;
-
-	queue<pair<int, int> > q_list;
-	q_list.push(make_pair(src, 0));
-
-	int curNode, curDist;
-	while (!q_list.empty()) {
-		curNode = q_list.front().first;
-		curDist = q_list.front().second;
-		q_list.pop();
-
-		int nextNode, nextDist;
-		for (int i = 0; i < v_pair[curNode].size(); i++) {
-			nextNode = v_pair[curNode][i].first;
-			nextDist = curDist + v_pair[curNode][i].second;
-
-			if (dist[nextNode] > nextDist) {
-				dist[nextNode] = nextDist;
-				q_list.push(make_pair(nextNode, nextDist));
-			}
-		}
-	}
-
-	printf("%d\n", dist[dst]);
 }
