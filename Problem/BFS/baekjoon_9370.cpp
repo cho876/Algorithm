0b5decb0294759b98be79a184a6fcc5662e0c281
skCho-PC\skCho <weapon114@naver.com>
2018-07-15 16:08:46 +0900

    <백준 문제풀이> 미확인 도착지

--- Problem/BFS/baekjoon_9370.cpp
+++ Problem/BFS/baekjoon_9370.cpp
@@ -3,88 +3,91 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <functional>
 
-#define INF 99999999
+#define INF 987654321
+
 using namespace std;
 
-int Dijkstra(vector<pair<int, int> >* v_list, int size, int src, int dst);
-
 vector<int> dist;
+vector<int> dstList;
 
-int main() {
-	int test;
-	scanf("%d", &test);
-	for (int i = 0; i < test; i++) {
-		int nodes, edges, candidates;
-		scanf("%d%d%d", &nodes, &edges, &candidates);
-		vector<pair<int, int> >* v_list = new vector<pair<int, int> >[nodes];
-
-		int src, already1, already2;
-		scanf("%d%d%d", &src, &already1, &already2);
-
-		int from, to, weight, already_weight;
-		for (int i = 0; i < edges; i++) {
-			scanf("%d%d%d", &from, &to, &weight);
-
-			if ((from == already1 && to == already2) || (from == already2 && to == already1))
-				already_weight = weight;
-			else {
-				v_list[from-1].push_back(make_pair(to-1, weight));
-				v_list[to-1].push_back(make_pair(from-1, weight));
-			}
-		}
-
-		vector<int> v_result;
-		
-		int route1 = Dijkstra(v_list, nodes, src - 1, already1 - 1) + already_weight;		
-		int route2 = Dijkstra(v_list, nodes, src - 1, already2 - 1) + already_weight;
-
-		
-		int cand = 0;
-		int base = 0;
-		int result = 0;
-		for (int i = 0; i < candidates; i++) {
-			scanf("%d", &cand);
-			base = Dijkstra(v_list, nodes, src - 1, cand-1);
-			result = min(route1 + Dijkstra(v_list, nodes, already2 - 1, cand - 1), route2 + Dijkstra(v_list, nodes, already1 - 1, cand - 1));
-			
-			if(base >= result)
-				v_result.push_back(cand);
-		}
-
-		sort(v_result.begin(), v_result.end());
-
-		for (int i = 0; i < v_result.size(); i++)
-			printf("%d ", v_result[i]);
-		printf("\n");
-	}
-	return 0;
-}
-
-int Dijkstra(vector<pair<int, int> >* v_list, int size, int src, int dst) {
-	dist.resize(size);
-	for (int i = 0; i < size; i++)
-		dist[i] = INF;
+vector<int> Dijkstra(vector<pair<int,int> >* v_adj, int nodes, int src) {
+	dist.resize(nodes);
+	fill(dist.begin(), dist.end(), INF);
 	dist[src] = 0;
 
-	priority_queue<pair<int, int> > q_list;
-	q_list.push(make_pair(src, 0));
+	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >path;
+	path.push({ 0, src });
 
 	int curNode, curWeight, nextNode, nextWeight;
-	while (!q_list.empty()) {
-		curNode = q_list.top().first;
-		curWeight = q_list.top().second;
-		q_list.pop();
+	while (!path.empty()) {
+		curWeight = path.top().first;
+		curNode = path.top().second;
+		path.pop();
 
-		for (int i = 0; i < v_list[curNode].size(); i++) {
-			nextNode = v_list[curNode][i].first;
-			nextWeight = curWeight + v_list[curNode][i].second;
+		if (dist[curNode] < curWeight)
+			continue;
+
+		for (int i = 0; i < v_adj[curNode].size(); i++) {
+			nextNode = v_adj[curNode][i].first;
+			nextWeight = curWeight + v_adj[curNode][i].second;
 
 			if (dist[nextNode] > nextWeight) {
 				dist[nextNode] = nextWeight;
-				q_list.push(make_pair(nextNode, nextWeight));
+				path.push({ nextWeight, nextNode });
 			}
 		}
 	}
-	return dist[dst];
+	return dist;
+}
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+	for (int i = 0; i < tc; i++) {
+		int nodes, edges, candidates;
+		scanf("%d%d%d", &nodes, &edges, &candidates);
+
+		int src, sect1, sect2;
+		scanf("%d%d%d", &src, &sect1, &sect2);
+		src--; sect1--; sect2--;
+
+		vector<pair<int, int> >* v_adj = new vector<pair<int, int> >[nodes];
+
+		int from, to, weight;
+		for (int i = 0; i < edges; i++) {
+			scanf("%d%d%d", &from, &to, &weight);
+			from--; to--;
+			v_adj[from].push_back({ to, weight });
+			v_adj[to].push_back({ from, weight });
+		}
+
+		dstList.clear();
+
+		int dst;
+		for (int i = 0; i < candidates; i++) {
+			scanf("%d", &dst);
+			dst--;
+			dstList.push_back(dst);
+		}
+
+		sort(dstList.begin(), dstList.end());
+		vector<int> dist1 = Dijkstra(v_adj, nodes, src);
+
+		int start = dist1[sect1] < dist1[sect2] ? sect2 : sect1;
+		vector<int> dist2 = Dijkstra(v_adj, nodes, start);
+
+		for (int i = 0; i < dstList.size(); i++) {
+			int here = dstList[i];
+
+			if (dist1[here] == INF || dist2[here] == INF)
+				continue;
+
+			if (dist1[here] == (dist1[start] + dist2[here]))
+				printf("%d ", here+1);
+		}
+		puts("");
+	}
+	return 0;
 }
