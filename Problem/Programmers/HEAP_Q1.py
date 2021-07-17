c6235092c32b13b0812d173f9636c16de3db7594
https://github.com/cho876/Algorithm.git <>
2021-07-18 00:15:11 +0900

    [프로그래머스] HEAP_Q1 - 더맵게

---
+++ Problem/Programmers/HEAP_Q1.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+
+    heap = []
+
+    for data in scoville:
+        heapq.heappush(heap, data)
+
+    while heap[0] < K:
+        try:
+            answer += 1
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
+        except:
+            return -1
+
+    return answer
