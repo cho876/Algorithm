b331e5d10628072e525fb3dd85a16ff8fee6aacf
https://github.com/cho876/Algorithm.git <>
2021-07-20 20:25:13 +0900

    [Programmers] 2021 kakao blind recruitment - 신규 아이디 추천

---
+++ Problem/Kakao/2021_kakao_blind_recruitment.py
@@ -0,0 +1,24 @@
+def solution(new_id):
+    answer = ''
+
+    sCh = new_id.lower()
+
+    for ch in sCh:
+        if ch.isalnum() or ch in '-_.':
+            answer += ch
+
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    answer = answer[1:] if len(answer) > 1 and answer[0] == '.' else answer
+    answer = answer[:-1] if answer[-1] == '.' else answer
+    answer = 'a' if len(answer) == 0 else answer
+
+    if len(answer) >= 16:
+        answer = answer[:15]
+    answer = answer[:-1] if answer[-1] == '.' else answer
+
+    while len(answer) < 3:
+        answer += answer[-1]
+
+    return answer
