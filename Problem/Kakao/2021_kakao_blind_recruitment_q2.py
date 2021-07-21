9e34afb83bb81559f571606c512963ffc7d9b3cf
https://github.com/cho876/Algorithm.git <>
2021-07-21 19:52:18 +0900

    [Programmers] 2021 Kakao Blind Recruitment Q2 - 메뉴 리뉴얼

---
+++ Problem/Kakao/2021_kakao_blind_recruitment_q2.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+
+    for c in course:
+        tmp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            tmp += combi
+
+        counter = Counter(tmp)
+
+        if len(counter) != 0 and max(counter.values()) > 1:
+            answer += [''.join(f) for f in counter if max(counter.values()) == counter[f]]
+    answer.sort()
+    return answer
