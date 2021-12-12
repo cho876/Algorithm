cafd6ba908a7848341ef9a7603642c103b726aa5
cho876 <weapon114@naver.com>
2021-12-12 15:09:25 +0900

    Programmers - 해시 문제풀이

---
+++ Problem/Programmers/Hash/Hash_Q2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+// 전화번호 목록
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        for(int i=0;i<phone_book.length-1;i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
